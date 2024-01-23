3c11c1c27589fc2c3248c807b28e060da30a1526
Tejas Pandit <tpandit79@gmail.com>
2024-01-23 22:22:25 +0530

    1

--- jenkins-env-test/INT.groovy
+++ jenkins-env-test/INT.groovy
@@ -1,7 +1,7 @@
 
 return [
     DATABASE_URL: 'jdbc:mysql://dev-db-server:3306/dev_database',
-    API_URL: 'http://dev-api-server:8080/api'
+    API_URL: 'http://dev-api-server:8080/api',
 
     // if (environment == 'dev') {
     //     imagename = "nginx"
